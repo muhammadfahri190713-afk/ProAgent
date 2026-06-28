29d1da1fd8d82c38bbf987573868a9ad51d37b0d
Newphone <muhammadfahri190713@gmail.com>
2026-06-28 16:15:52 +0700

    Create secrets.js for obfuscated configuration
    Add auto-generated secrets module with obfuscated values.

---
+++ src/secrets.js
@@ -0,0 +1,7 @@
+'use strict';
+// ============================================================
+//  SECRETS — AUTO-GENERATED. JANGAN edit manual.
+//  Generate ulang: npm run encode
+//  Nilai asli ter-obfuscate (XOR + Base64). Aman buat di-public.
+// ============================================================
+module.exports = { BLOB: "C3BxES5sJTEGVAkYSRhIMRoCFndFdFVcOCJlAF9mID8nDAVUSlFJQTA8IXN9eXxiUmgSKRNHHgdlWRxbShpLDhQcEFAcWV8ZESJZbgtfA1kvBFxCQ1wTUloRDUJGH1FZHSJcJBNaARosVB8YaSY1IjczNmZtZWB6UmgSY0sRPSEPN3F7aTY6Ijs9K3x5dWsUSnASbUV8OToaJGx/dzIsL1dIR0xFXldEMCJCLgZUCxorWFpeGA4=" };
